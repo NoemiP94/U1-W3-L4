ee644c5cc8027330abcf63355cbc0b5bab98ab68
Noemi Pusceddu <m_emi94@hotmail.it>
2023-09-14 15:55:36 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,36 @@
+//creare le celle della tabella
+
+const cells = 76
+
+const createCells = function (cells) {
+  const tableDiv = document.getElementById('tabellone') //recupero il div dove va messa la tabella
+  for (let i = 1; i <= cells; i++) {
+    const cellDiv = document.createElement('div') // creo div vuoto
+    cellDiv.classList.add('cell') //aggiungo la classe 'cell'
+    const cellValue = document.createElement('h3') //creo il numero da inserire nella cella
+    cellValue.innerText = i
+
+    cellDiv.appendChild(cellValue)
+    tableDiv.appendChild(cellDiv)
+  }
+}
+
+createCells(cells)
+
+//estrazione di un numero random dal bottone
+
+const extract = function () {
+  const random = Math.floor(Math.random() * 77) //numero random
+  //collegare random alla cella giusta
+
+  const selectedNumber = []
+  selectedNumber.push(random)
+
+  selectedNumber.classList.add('selected')
+}
+const button = document.getElementById('extraction')
+button.addEventListener('click', extract)
+extract()
+
+// evidenziare il numero selezionto dopo il click del bottone
+//creo array dove inserire i numeri selezionati
